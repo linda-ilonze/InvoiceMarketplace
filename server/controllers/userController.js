e0697027d855fb24c684a54b92bb23cdebc706dd
toluhi <toluhi@yahoo.com>
2015-05-16 16:29:48 +0100

    added user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yemi-t on 16/05/15.
+ */
+
+var User = require('mongoose').model('User');
---
+++ server/routes/users.routes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yemi-t on 16/05/15.
+ */
+var users = require('../controllers/userController');
+
+module.exports = function(app) {
+
+    app.route('/users')
+        .post(users.create)
+        .get(users.list);
+
+}
